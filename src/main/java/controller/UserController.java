48bfd6599cf4584cedf08c905ad1a6c821679b71
josevillacres46 <jvillacr@ucm.es>
2025-04-04 15:51:29 +0200

    FEAT: Add userService

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -9,17 +10,26 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import business.dto.CreateUserDTO;
+import business.services.UserService;
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserController {
     
+    private UserService userService;
+
     @POST
     @Transactional
     public Response createUser(CreateUserDTO createUserDTO) {
+        this.userService.beginCreateUser(createUserDTO);
         return Response.status(Response.Status.CREATED)
                         .entity("User created successfully").build();
+    }
+
+    @Inject
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
 }
