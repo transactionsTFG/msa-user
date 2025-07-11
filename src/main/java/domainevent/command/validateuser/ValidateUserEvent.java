44d0ef232be93e5ef39dadbfbd96be045e5097f1
josevillacres46 <jvillacr@ucm.es>
2025-07-11 18:48:57 +0200

    FIX: Remove Agency Command

--- src/main/java/domainevent/command/validateuser/ValidateUserEvent.java
+++ src/main/java/domainevent/command/validateuser/ValidateUserEvent.java
@@ -7,7 +7,7 @@ import business.qualifier.ValidateUserQualifier;
 import business.user.UserDTO;
 import domainevent.command.handler.BaseHandler;
 import domainevent.command.handler.EventHandler;
-import msa.commons.commands.agency.reservationairline.AgencyReservationAirlineCommmad;
+import msa.commons.commands.createreservation.CreateReservationCommand;
 import msa.commons.event.EventData;
 import msa.commons.event.EventId;
 import msa.commons.event.eventoperation.EventOperation;
@@ -26,9 +26,9 @@ public class ValidateUserEvent extends BaseHandler {
     }
 
     private void handleCreateReservationAirline(String json) {
-        EventData event = EventData.fromJson(json, AgencyReservationAirlineCommmad.class);
-        AgencyReservationAirlineCommmad command = (AgencyReservationAirlineCommmad) event.getData();
-        UserDTO user = this.userService.getUserByEmail(command.getCreateReservationCommand().getCustomerInfo().getEmail());
+        EventData event = EventData.fromJson(json, CreateReservationCommand.class);
+        CreateReservationCommand command = (CreateReservationCommand) event.getData();
+        UserDTO user = this.userService.getUserByEmail(command.getCustomerInfo().getEmail());
         if (user == null) 
             return;
         this.jmsEventDispatcher.publish(EventId.RESERVATION_AIRLINE_CREATE_RESERVATION_BEGIN_SAGA, user, ReservationAirline.CREATE_RESERVATION_ONLY_AIRLINE);
