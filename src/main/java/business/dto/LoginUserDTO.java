a94ca58a343695946d9177bc7ffc859305b42be5
josevillacres46 <jvillacr@ucm.es>
2025-09-12 20:31:58 +0200

    mod

--- src/main/java/business/dto/LoginUserDTO.java
+++ src/main/java/business/dto/LoginUserDTO.java
@@ -2,9 +2,11 @@ package business.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor 
 public class LoginUserDTO {
     private String email;
     private String password;
